d3649ac311efd013bc1bb9635c6f571915062799
TheCrether <thecrether@gmail.com>
2022-08-11 13:29:15 +0200

    :zap: improved performance for sidebar animations

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,10 +1,14 @@
 class Sidebar {
-	constructor () {
-		this._sidebar = document.querySelector('#sidebar');
-		this._sidebarOverlay = document.querySelector('#sidebar-overlay');
-		this._buttonMainScreenSettingsImage = document.querySelector('#button-main-screen-settings-image');
-		this._buttonMainScreenPowersImage = document.querySelector('#button-main-screen-powers-image');
-		this._passwordInput = document.querySelector('#input-password');
+	constructor() {
+		this._sidebar = document.querySelector("#sidebar");
+		this._sidebarOverlay = document.querySelector("#sidebar-overlay");
+		this._buttonMainScreenSettingsImage = document.querySelector(
+			"#button-main-screen-settings-image"
+		);
+		this._buttonMainScreenPowersImage = document.querySelector(
+			"#button-main-screen-powers-image"
+		);
+		this._passwordInput = document.querySelector("#input-password");
 		this._sidebarVisible = false;
 		this._init();
 	}
@@ -19,19 +23,25 @@ class Sidebar {
 
 	showSidebar() {
 		this._passwordInput.blur();
-		this._sidebar.classList.add('sidebar-show');
-		this._sidebarOverlay .classList.add('sidebar-overlay-show');
-		this._buttonMainScreenSettingsImage.classList.add('button-main-screen-blur');
-		this._buttonMainScreenPowersImage.classList.add('button-main-screen-blur');
+		this._sidebar.classList.add("sidebar-show");
+		this._sidebarOverlay.classList.add("sidebar-overlay-show");
+		this._buttonMainScreenSettingsImage.classList.add(
+			"button-main-screen-blur"
+		);
+		this._buttonMainScreenPowersImage.classList.add("button-main-screen-blur");
 		this._sidebarVisible = true;
 	}
 
 	hideSidebar() {
 		this._passwordInput.focus();
-		this._sidebar.classList.remove('sidebar-show');
-		this._sidebarOverlay .classList.remove('sidebar-overlay-show');
-		this._buttonMainScreenSettingsImage.classList.remove('button-main-screen-blur');
-		this._buttonMainScreenPowersImage.classList.remove('button-main-screen-blur');
+		this._sidebar.classList.remove("sidebar-show");
+		this._sidebarOverlay.classList.remove("sidebar-overlay-show");
+		this._buttonMainScreenSettingsImage.classList.remove(
+			"button-main-screen-blur"
+		);
+		this._buttonMainScreenPowersImage.classList.remove(
+			"button-main-screen-blur"
+		);
 		this._sidebarVisible = false;
 	}
 
@@ -45,11 +55,10 @@ class Sidebar {
 	}
 
 	_sidebarOverlayOnClickEvent() {
-		this._sidebarOverlay.addEventListener(
-			'click',
-			() => {
+		this._sidebarOverlay.addEventListener("click", () => {
+			if (this._sidebarVisible) {
 				this.hideSidebar();
 			}
-		);
+		});
 	}
 }
